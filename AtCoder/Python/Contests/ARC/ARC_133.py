9fa1bf83a16975b9d7b9641db929f3bcc76a9ae7
sougo0811 <sougo2813@outlook.jp>
2022-01-22 23:48:48 +0900

    2022/01/22

--- AtCoder/Python/Contests/ARC/ARC_133.py
+++ AtCoder/Python/Contests/ARC/ARC_133.py
@@ -1,15 +1,24 @@
+import collections
+
 N = int(input())
 AN = list(map(int,input().split()))
 M = 200000
 BN = list(range(1,200001))
 cnt = 0
 ans = [20001]
-for i in range(N):
-  if cnt <= AN.count(i):
+
+c = collections.Counter(AN)
+c = dict(c)
+max_k_list = [kv[0] for kv in c.items() if kv[1] == max(c.values())]
+#print(max_k_list)
+
+for i in (max_k_list):
+  if cnt <= i:
     tmp = [j for j in AN if j != i]
     if ans >= tmp:
       ans = tmp
-      cnt = AN.count(i)
+      cnt = i
+
 
 ans = " ".join(map(str,ans))
 print(ans)
