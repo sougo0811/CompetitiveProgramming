e614dcec0fc13496d636be6b6d659cd7de1f4414
sougo0811 <sougo2813@outlook.jp>
2022-01-31 00:32:48 +0900

    2022/01/31

---
+++ AtCoder/Python/Contests/ABC/ABC_237.py
@@ -0,0 +1,27 @@
+#A
+'''
+N = int(input())
+if  -2**31<=N<2**31:
+  print("Yes")
+else:
+  print("No")
+'''
+#B
+import numpy as np
+H,W = map(int,input().split())
+AHW = []
+for _ in range(H):
+  AW = list(map(int,input().split()))
+  AHW.append(AW)
+
+AHW_t = (np.array(AHW).T).tolist()
+for i in range(W):
+  ans = " ".join(map(str,AHW_t[i]))
+  print(ans)
+
+
+'''
+AHW_t = np.array(AHW).T
+for i in AHW_t:
+  print(*i)
+'''
