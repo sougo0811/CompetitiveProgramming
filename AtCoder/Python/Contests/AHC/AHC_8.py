27346c14b55623a34dc9285eb1771c422c7fb6e7
sougo0811 <sougo2813@outlook.jp>
2022-02-26 20:49:45 +0900

    2022/02/26

---
+++ AtCoder/Python/Contests/AHC/AHC_8.py
@@ -0,0 +1,18 @@
+N = int(input())
+pxN = []
+pyN = []
+ptN = []
+for _ in range(N):
+  px,py,pt = map(int,input().split())
+  pxN.append(px)
+  pyN.append(py)
+  ptN.append(pt)
+
+M = int(input())
+hxM = []
+hyM = []
+for _ in range(M):
+  hx,hy = map(int,input().split())
+  hxM.append(hx)
+  hyM.append(hy)
+
